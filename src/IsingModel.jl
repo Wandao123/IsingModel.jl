7e6dc5644a3e26e3f4155a9aabf7cb820e18edaf
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2023-07-19 18:12:13 +0800

    Add updating algorithms for a Ising-spin system
    The current interface to initialize each struct is developed still.  It
    is not useful to receive an adjacency matrix from a user because all
    structs accept any matrices for now.  One has to send a symmetric
    matrix whose all diagonal components are zero.  To improve this
    interface, we should make a restriction that a user passes either
    upper or lower triangular parts of off-diagonal components.

--- src/IsingModel.jl
+++ src/IsingModel.jl
@@ -1,5 +1,88 @@
 module IsingModel
 
-# Write your package code here.
+export SpinSystem
+export calcEnergy
+export AsynchronousHopfieldNetwork
+export GlauberDynamics
+export MetropolisMethod
+export update!
+
+using Random, Distributions
+
+@enum IsingSpin DownSpin = -1 UpSpin = +1
+
+mutable struct SpinSystem
+    spinConfiguration::Vector{Number}
+    couplingCoefficients::Matrix{AbstractFloat}  # Like an adjacency matrix
+    externalMagneticField::Vector{AbstractFloat}  # Bias on each site
+end
+
+"""
+    UpdatingAlgorithm
+
+Suppose that any sub-struct of this type has the spinSystem::SpinSystem field.
+"""
+abstract type UpdatingAlgorithm end
+
+function calcEnergy(spinSystem::SpinSystem)::AbstractFloat
+    return -0.5 * spinSystem.spinConfiguration' * spinSystem.couplingCoefficients * spinSystem.spinConfiguration
+        - spinSystem.externalMagneticField' * spinSystem.spinConfiguration
+end
+
+calcEnergy(s::UpdatingAlgorithm)::AbstractFloat = calcEnergy(s.spinSystem)
+
+function calcLocalMagneticField(spinSystem::SpinSystem)::Vector{AbstractFloat}
+    return spinSystem.couplingCoefficients * spinSystem.spinConfiguration
+        + spinSystem.externalMagneticField
+end
+
+calcLocalMagneticField(s::UpdatingAlgorithm)::Vector{AbstractFloat} = calcLocalMagneticField(s.spinSystem)
+
+mutable struct AsynchronousHopfieldNetwork <: UpdatingAlgorithm
+    spinSystem::SpinSystem
+end
+
+function update!(s::AsynchronousHopfieldNetwork)
+    x = rand(eachindex(s.spinSystem.spinConfiguration))  # An updated node label
+    s.spinSystem.spinConfiguration[x] = sign(
+            # HACK: Should we point out the index x before calculating matrices?
+            (s.spinSystem.couplingCoefficients * s.spinSystem.spinConfiguration
+            - s.spinSystem.externalMagneticField)[x]
+        ) |> Int
+end
+
+mutable struct GlauberDynamics <: UpdatingAlgorithm
+    spinSystem::SpinSystem
+    temperature::AbstractFloat
+end
+
+function update!(s::GlauberDynamics)
+    if s.temperature < 0
+        @warn "$temperature is negative."
+    end
+
+    x = rand(eachindex(s.spinSystem.spinConfiguration))  # An updated node label
+    s.spinSystem.spinConfiguration[x] = sign(
+            2 * calcLocalMagneticField(s)[x]
+            - rand(Logistic()) * s.temperature
+        ) |> Int
+end
+
+mutable struct MetropolisMethod <: UpdatingAlgorithm
+    spinSystem::SpinSystem
+    temperature::AbstractFloat
+end
+
+function update!(s::MetropolisMethod)
+    if s.temperature < 0
+        @warn "$temperature is negative."
+    end
+
+    x = rand(eachindex(s.spinSystem.spinConfiguration))  # An updated node label
+    s.spinSystem.spinConfiguration[x] = sign(
+            2 * calcLocalMagneticField(s)[x]
+            - rand(Exponential()) * s.temperature * s.spinSystem.spinConfiguration[x]
+        ) |> Int
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using IsingModel
 using Test
 
 @testset "IsingModel.jl" begin
-    # Write your tests here.
+    s = SpinSystem([-1, +1], [0 1; 1 0], [0, 0])
+    println(calcEnergy(s))
+    println(IsingModel.calcLocalMagneticField(s))
+    a = AsynchronousHopfieldNetwork(deepcopy(s))
+    update!(a)
+    a = GlauberDynamics(deepcopy(s), 10.0)
+    update!(a)
+    a = MetropolisMethod(deepcopy(s), 10.0)
+    update!(a)
 end
