50546b3527147b2b45db2e5da0e1629a811f0df5
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2023-10-24 18:32:27 +0800

    Add the same Jupyter notebook for demonstrations as the Pluto notebook

--- demo.jl
+++ demo.jl
@@ -5,17 +5,6 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 3db0f7f0-26b8-11ee-345a-970b2c1cf2ed
-# Before running this demonstration, install python libraries via REPL:
-# `julia> using Conda`
-# `julia> Conda.add("scipy")`
-# `julia> Conda.add("networkx")`
-# Depending on your OS, you may be required to chenge
-# the path to Python interpreter and build PyCall.
-# `julia> ENV["PYTHON"] = ENV["HOME"] * "/.julia/conda/3/x86_64/bin/python3"`
-# `julia> using Pkg`
-# `julia> Pkg.build("PyCall")`
-# See also https://github.com/JuliaPy/PyCall.jl
-
 # Ref: https://stackoverflow.com/questions/70974016/julia-pluto-cannot-find-dev-installed-package
 begin
 	import Pkg
@@ -27,8 +16,9 @@ begin
 	using IsingModel
 	using BenchmarkTools
 	using Conda
-	using Graphs, GraphPlot
+	#using Graphs, GraphPlot
 	using LinearAlgebra
+	using Plots
 	using PyCall
 	using Random, Distributions
 	using Revise
@@ -39,6 +29,11 @@ begin
 	#Conda.add("networkx")
 	const nx = pyimport("networkx")
 end
+
+# ╔═╡ 4f999a32-2d07-45c7-a30f-2f87c65a053a
+md"""
+# Demonstrations
+"""
 
 # ╔═╡ 8c380ce4-4e41-43d9-afb2-e2252f58e5e4
 # Ref: https://github.com/JuliaPy/PyCall.jl/issues/204
@@ -56,60 +51,72 @@ const N = 64  # The number of nodes
 # ╔═╡ 3b95e78f-0743-4a3e-b334-2ced85113b89
 # Generate a square lattice with the periodic boundary condition.
 begin
-	const sideLength = (Int ∘ ceil ∘ sqrt)(N)
+	const SIDE_LENGTH = (Int ∘ ceil ∘ sqrt)(N)
 
 	# Directly making the adjacency matrix
 	#=adjacencyMatrix = zeros((N, N))
 	for i = 0:N-1
-		if i % sideLength < sideLength - 1 && i + 1 < N
+		if i % SIDE_LENGTH < SIDE_LENGTH - 1 && i + 1 < N
 			adjacencyMatrix[i + 1, (i + 1) + 1] = 1
 		else
-			adjacencyMatrix[(i ÷ sideLength) * sideLength + 1, i + 1] = 1
+			adjacencyMatrix[(i ÷ SIDE_LENGTH) * SIDE_LENGTH + 1, i + 1] = 1
 		end
-		if i ÷ sideLength < (N - 1) ÷ sideLength
-			adjacencyMatrix[i + 1, i + sideLength + 1] = 1
+		if i ÷ SIDE_LENGTH < (N - 1) ÷ SIDE_LENGTH
+			adjacencyMatrix[i + 1, i + SIDE_LENGTH + 1] = 1
 		else
-			adjacencyMatrix[i % sideLength + 1, i + 1] = 1
+			adjacencyMatrix[i % SIDE_LENGTH + 1, i + 1] = 1
 		end
 	end
 	adjacencyMatrix = Symmetric(adjacencyMatrix, :U)=#
 
-	display(adjacency_matrix(grid((sideLength, sideLength))))  # Another way
+	# A way using the Graphs.jl library
+	#adjacencyMatrix = adjacency_matrix(grid((SIDE_LENGTH, SIDE_LENGTH), periodic=true))
 
-	G = nx.grid_2d_graph(sideLength, sideLength, periodic=true)
+	G = nx.grid_2d_graph(SIDE_LENGTH, SIDE_LENGTH, periodic=true)
 	nx.set_edge_attributes(G, values=-1, name="weight")
-	const adjacencyMatrix = mysparse(nx.adjacency_matrix(G))
+	adjacencyMatrix = mysparse(nx.adjacency_matrix(G))
 end
 
 # ╔═╡ 2d3c39e0-c31b-4741-bc85-4d00082f64c4
-const bias = zeros(N)
+bias = zeros(N)
 
 # ╔═╡ 1fdecb38-6c88-4c15-bd62-836e77b0170a
-const initialConfiguration = 2 .* rand(Bernoulli(0.5), N) .- 1
+const INITIAL_CONFIGURATION = 2 .* rand(Bernoulli(0.5), N) .- 1
 
 # ╔═╡ 9539349c-1cdb-4bc0-8d9e-94406a96e49c
-spinSystem = SpinSystem(initialConfiguration, adjacencyMatrix, bias)
+spinSystem = SpinSystem(INITIAL_CONFIGURATION, adjacencyMatrix, bias)
 
 # ╔═╡ 5caf6d8b-8e0a-4e49-9ada-d7a403ca04de
 begin
-	const maxSteps = N ^ 2
-	const initialTemperature = float(maxSteps)
-	const finalTemperature = 0.0
+	const MAX_STEPS = N ^ 2
+	const INITIAL_TEMPERATURE = float(MAX_STEPS)
+	const FINAL_TEMPERATURE = 0.0
 
-	#annealingSchedule(n) = (finalTemperature - initialTemperature) / maxSteps * n + initialTemperature
-	annealingSchedule(n) = initialTemperature ^ (-n)
+	#annealingSchedule(n) = (FINAL_TEMPERATURE - INITIAL_TEMPERATURE) / MAX_STEPS * n + INITIAL_TEMPERATURE
+	annealingSchedule(n) = INITIAL_TEMPERATURE ^ (-n)
+
+	function runAnnealer(Algorithm::Type{<:IsingModel.UpdatingAlgorithm}, spinSystem::SpinSystem, INITIAL_TEMPERATURE=-1.0)::Vector{Float64}
+	    if INITIAL_TEMPERATURE < 0.0
+	        return map(calcEnergy, takeSamples!(Algorithm(deepcopy(spinSystem)), MAX_STEPS, annealingSchedule))
+	    else
+	        return map(calcEnergy, takeSamples!(Algorithm(deepcopy(spinSystem), INITIAL_TEMPERATURE), MAX_STEPS, annealingSchedule))
+    	end
+	end
 end
 
 # ╔═╡ b14cb2a9-d69f-48eb-a268-6bca0ff91675
-@benchmark begin
-	#algorithm = AsynchronousHopfieldNetwork(deepcopy(spinSystem))
-	algorithm = GlauberDynamics(deepcopy(spinSystem), initialTemperature)
-	#algorithm = MetropolisMethod(deepcopy(spinSystem), initialTemperature)
-	data = takeSamples!(algorithm, maxSteps, annealingSchedule)
-	println(map(calcEnergy, data))
+@benchmark runAnnealer(GlauberDynamics, spinSystem, INITIAL_TEMPERATURE)
+
+# ╔═╡ 34e062e0-e878-4f48-a7fd-91009b83be0f
+begin
+	plot(xlabel="MC steps", ylabel="Energy")
+	plot!(runAnnealer(AsynchronousHopfieldNetwork, spinSystem), label="Hopfield")
+	plot!(runAnnealer(GlauberDynamics, spinSystem, INITIAL_TEMPERATURE), label="Glauber")
+	plot!(runAnnealer(MetropolisMethod, spinSystem, INITIAL_TEMPERATURE), label="Metropolis")
 end
 
 # ╔═╡ Cell order:
+# ╟─4f999a32-2d07-45c7-a30f-2f87c65a053a
 # ╠═3db0f7f0-26b8-11ee-345a-970b2c1cf2ed
 # ╠═8c380ce4-4e41-43d9-afb2-e2252f58e5e4
 # ╠═9fb8f983-0fae-4fbd-bf6f-3a788323f25f
@@ -119,3 +126,4 @@ end
 # ╠═9539349c-1cdb-4bc0-8d9e-94406a96e49c
 # ╠═5caf6d8b-8e0a-4e49-9ada-d7a403ca04de
 # ╠═b14cb2a9-d69f-48eb-a268-6bca0ff91675
+# ╠═34e062e0-e878-4f48-a7fd-91009b83be0f
