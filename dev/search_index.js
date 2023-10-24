5976869177e8bf0e5409220088ab89feaf16487a
Documenter.jl <documenter@juliadocs.github.io>
2023-10-24 17:49:56 +0000

    build based on 495f144

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IsingModel","category":"page"},{"location":"#IsingModel","page":"Home","title":"IsingModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IsingModel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IsingModel]","category":"page"}]
+}
